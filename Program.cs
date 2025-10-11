08d871f473ac9dda1619bcd6ed64f7ef44743639
Nick Moghadam <nickmo96@hotmail.com>
2025-10-12 00:56:21 +0200

    første forsøg på bot

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿internal class Program
+{
+    private async static Task Main(string[] args)
+    {
+        using var client = new HttpClient();
+        var request = new HttpRequestMessage(
+            HttpMethod.Post,
+            "https://api.telegram.org/bot8393432003:AAFCByo9c06ZvAd2U1SHvDo-bq-h26sp-M8/sendMessage?chat_id=7091701318&text=din fars"
+        );
+
+        var response = await client.SendAsync(request);
+        var content = await response.Content.ReadAsStringAsync();
+
+        Console.WriteLine(response.StatusCode);
+        Console.WriteLine(content);
+    }
+}
