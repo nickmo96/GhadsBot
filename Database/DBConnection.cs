6249870db4b4fbaef4722c9069f13cee29949384
nickmo96 <nickmo96@hotmail.com>
2025-10-15 20:40:13 +0200

    app.config tilføjet hva

--- Database/DBConnection.cs
+++ Database/DBConnection.cs
@@ -3,14 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Reflection;
+using System.Configuration;
+
 
 namespace GhadsBot.Database
 {
     public class DBConnection
     {
-        private readonly string _connectionString = "Data Source=localhost;Initial Catalog = GhadsBot; User ID = sa; Password=***********;Trust Server Certificate=True";
-        public DBConnection(string _connectionString) { 
-            this._connectionString = _connectionString;
+        private readonly string _connectionString = ConfigurationManager.ConnectionStrings["LocalConnection"].ConnectionString.ToString();
+        private static DBConnection? _instance;
+        public DBConnection() { 
+            
         }
 
         
